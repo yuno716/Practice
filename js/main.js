51e4c2b99fcf3818c51cd8131be77ce08ca414cb
yuno <dafujunei@gmail.com>
2019-11-17 21:53:38 +0900

    整数以外が入力されたときに整数値を入力してくださいを表示させる。

--- js/main.js
+++ js/main.js
@@ -5,22 +5,31 @@
     const fizzNum = document.querySelector('input[name="text1"]');
     const buzzNum = document.querySelector('input[name="text2"]');
 
-    for (let i = 1; i <= 100; i++) {
-      if(i % fizzNum.value === 0 && i % buzzNum.value === 0) {
-        const p = document.createElement('p');
-        p.textContent = 'FizzBuzz'+ i;
-        document.body.appendChild(p);
-      }else if(i % fizzNum.value === 0) {
-        const p = document.createElement('p');
-        p.textContent = 'Fizz'+ i;
-        document.body.appendChild(p);
-      }else if(i % buzzNum.value === 0) {
-        const p = document.createElement('p');
-        p.textContent = 'Buzz'+ i;
-        document.body.appendChild(p);
-      }
-    }
+    const Num = parseFloat(fizzNum.value, buzzNum.value);
+    // const fizz = parseFloat(fizzNum.value);
+    // const buzz = parseFloat(buzzNum.value);
 
+    if (Number.isInteger(Num)){
+      for (let i = 1; i <= 100; i++) {
+        if (i % fizzNum.value === 0 && i % buzzNum.value === 0) {
+          const p = document.createElement('p');
+          p.textContent = 'FizzBuzz'+ i;
+          document.body.appendChild(p);
+        }else if (i % fizzNum.value === 0) {
+          const p = document.createElement('p');
+          p.textContent = 'Fizz'+ i;
+          document.body.appendChild(p);
+        }else if (i % buzzNum.value === 0) {
+          const p = document.createElement('p');
+          p.textContent = 'Buzz'+ i;
+          document.body.appendChild(p);
+        }
+      }
+    }else {
+      const p = document.createElement('p');
+      p.textContent = '整数値を入力してください';
+      document.body.appendChild(p);
+    }
   });
   
 }
