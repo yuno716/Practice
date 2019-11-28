19dbda8e7f08dbe05aa52f7586a794444c811c09
yuno <dafujunei@gmail.com>
2019-11-28 14:59:11 +0900

    Revert "textContent 追加"
    This reverts commit ff49151cab302ca4ac5ce4f420acef52102004f9.

--- js/main.js
+++ js/main.js
@@ -2,37 +2,33 @@
 'use strict'
 {
   document.getElementById('btn').addEventListener('click',() => {
-    const output = document.getElementById('output');
-
-    output.textContent = null;
-
     const fizzNum = document.querySelector('input[name="text1"]');
     const buzzNum = document.querySelector('input[name="text2"]');
 
     const fizz = parseFloat(fizzNum.value);
     const buzz = parseFloat(buzzNum.value);
 
-    if (Number.isInteger(fizz && buzz)){
+    if (Number.isInteger(fizz, buzz)){
       for (let i = 1; i <= 100; i++) {
         if (i % fizzNum.value === 0 && i % buzzNum.value === 0) {
           const p = document.createElement('p');
           p.textContent = 'FizzBuzz'+ i;
-          output.appendChild(p);
+          document.body.appendChild(p);
         }else if (i % fizzNum.value === 0) {
           const p = document.createElement('p');
           p.textContent = 'Fizz'+ i;
-          output.appendChild(p);
+          document.body.appendChild(p);
         }else if (i % buzzNum.value === 0) {
           const p = document.createElement('p');
           p.textContent = 'Buzz'+ i;
-          output.appendChild(p);
+          document.body.appendChild(p);
         }
       }
     }else {
       const p = document.createElement('p');
       p.textContent = '整数値を入力してください';
-      output.appendChild(p);
+      document.body.appendChild(p);
     }
   });
-
+  
 }
