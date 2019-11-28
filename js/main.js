ff49151cab302ca4ac5ce4f420acef52102004f9
yuno <dafujunei@gmail.com>
2019-11-28 14:58:44 +0900

    textContent 追加

--- js/main.js
+++ js/main.js
@@ -2,33 +2,37 @@
 'use strict'
 {
   document.getElementById('btn').addEventListener('click',() => {
+    const output = document.getElementById('output');
+
+    output.textContent = null;
+
     const fizzNum = document.querySelector('input[name="text1"]');
     const buzzNum = document.querySelector('input[name="text2"]');
 
     const fizz = parseFloat(fizzNum.value);
     const buzz = parseFloat(buzzNum.value);
 
-    if (Number.isInteger(fizz, buzz)){
+    if (Number.isInteger(fizz && buzz)){
       for (let i = 1; i <= 100; i++) {
         if (i % fizzNum.value === 0 && i % buzzNum.value === 0) {
           const p = document.createElement('p');
           p.textContent = 'FizzBuzz'+ i;
-          document.body.appendChild(p);
+          output.appendChild(p);
         }else if (i % fizzNum.value === 0) {
           const p = document.createElement('p');
           p.textContent = 'Fizz'+ i;
-          document.body.appendChild(p);
+          output.appendChild(p);
         }else if (i % buzzNum.value === 0) {
           const p = document.createElement('p');
           p.textContent = 'Buzz'+ i;
-          document.body.appendChild(p);
+          output.appendChild(p);
         }
       }
     }else {
       const p = document.createElement('p');
       p.textContent = '整数値を入力してください';
-      document.body.appendChild(p);
+      output.appendChild(p);
     }
   });
-  
+
 }
