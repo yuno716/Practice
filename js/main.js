ccd4e99a2ef9217eef9da8d43565a0b2c02f9658
yuno <dafujunei@gmail.com>
2019-11-29 07:06:26 +0900

    textContent = null 追加

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@
 'use strict'
 {
   document.getElementById('btn').addEventListener('click',() => {
+    const output = document.getElementById('output');
+
+    output.textContent = null;
+
     const fizzNum = document.querySelector('input[name="text1"]');
     const buzzNum = document.querySelector('input[name="text2"]');
 
@@ -13,21 +17,21 @@
         if (i % fizzNum.value === 0 && i % buzzNum.value === 0) {
           const p = document.createElement('p');
           p.textContent = 'FizzBuzz'+ i;
-          document.body.appendChild(p);
+          output.appendChild(p);
         }else if (i % fizzNum.value === 0) {
           const p = document.createElement('p');
           p.textContent = 'Fizz'+ i;
-          document.body.appendChild(p);
+          output.appendChild(p);
         }else if (i % buzzNum.value === 0) {
           const p = document.createElement('p');
           p.textContent = 'Buzz'+ i;
-          document.body.appendChild(p);
+          output.appendChild(p);
         }
       }
     }else {
       const p = document.createElement('p');
       p.textContent = '整数値を入力してください';
-      document.body.appendChild(p);
+      output.appendChild(p);
     }
   });
   
